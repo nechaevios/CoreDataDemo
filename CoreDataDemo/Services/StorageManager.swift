44bc31b66c2428f6a30a45d93ae7f1b6e4d6b3ec
Sergey <21218903+xchakiex@users.noreply.github.com>
2021-10-06 15:36:12 +0300

    core manager added

---
+++ CoreDataDemo/Services/StorageManager.swift
@@ -0,0 +1,41 @@
+//
+//  StorageManager.swift
+//  CoreDataDemo
+//
+//  Created by Nechaev Sergey  on 06.10.2021.
+//
+
+import CoreData
+
+class StorageManager {
+    
+    static let shared = StorageManager()
+    
+    private init() {}
+    
+    // MARK: - Core Data stack
+    var persistentContainer: NSPersistentContainer = {
+        let container = NSPersistentContainer(name: "CoreDataDemo")
+        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
+            if let error = error as NSError? {
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+            }
+        })
+        return container
+    }()
+    
+    // MARK: - Core Data Saving support
+    
+    func saveContext () {
+        let context = persistentContainer.viewContext
+        if context.hasChanges {
+            do {
+                try context.save()
+            } catch {
+                let nserror = error as NSError
+                fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+            }
+        }
+    }
+    
+}
